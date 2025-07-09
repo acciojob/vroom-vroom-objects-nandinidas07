938e725a77dff8fb07937b0b1b51098ee75ff1d3
nandinidas07 <nandinidas107@gmail.com>
2025-07-09 18:36:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
   this.make = make;
   this.model = model;
 }
@@ -8,20 +8,17 @@ Car.prototype.getMakeModel = function () {
   return this.make + " " + this.model;
 };
 
-
-
-function SportsCar(make, model, topSpeed) {}
-Car.call(this, make, model); // Inherit properties from Car
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model); // Inherit from Car
   this.topSpeed = topSpeed;
 }
 
-SportsCar.prototype = Object.create(Car.prototype); // Inherit methods
+SportsCar.prototype = Object.create(Car.prototype); // Inherit prototype
 SportsCar.prototype.constructor = SportsCar;
 
 SportsCar.prototype.getTopSpeed = function () {
   return this.topSpeed;
 };
-
 
 // Do not change the code below
 window.Car = Car;
